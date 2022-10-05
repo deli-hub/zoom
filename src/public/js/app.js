f99b92fb8132243e100e3b6203e77e8692b16733
deli-hub <deli8908@gmail.com>
2022-10-05 16:57:49 +0900

    webSocket -> SocketIO 변경

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,62 +1,91 @@
+/*
+WebSocket에서 사용
 const messageList = document.querySelector("ul");
 const nickForm = document.querySelector("#nick");
 const messageForm = document.querySelector("#message");
-/**socket에서 object가 아닌 string으로 굳이 변환을 해서 보내주는 이유는
+
+socket에서 object가 아닌 string으로 굳이 변환을 해서 보내주는 이유는
  * websocket이 브라우저에 있는 API이기 때문이다.
  * 백엔드에서는 다양한 프로그래밍 언어를 사용할 수 있기 때문에
  * API는 어떠한 판단도 해서는 안된다.
- */
-const socket = new WebSocket(`ws://${window.location.host}`);
 
-function makeMessage(type, payload) {
-    const msg = {type, payload}
-    return JSON.stringify(msg);
+ const socket = new WebSocket(`ws://${window.location.host}`);
+
+ function makeMessage(type, payload) {
+     const msg = {type, payload}
+     return JSON.stringify(msg);
+ }
+ 
+ socket.addEventListener("open", () => {
+     console.log("Connected to Server ✅");
+ });
+ 
+ socket.addEventListener("message", (message) => {
+     // console.log("Just got this: ", message, "from the server");
+     // console.log("New Message: ", message.data);
+     const li = document.createElement("li");
+     li.innerText = message.data;
+     messageList.append(li); 
+ });
+ 
+ socket.addEventListener("close", () => {
+     console.log("Disconnected from Server ❌");
+ });
+ 
+ // 첫 번째 줄은 JSON 형태가 아니기 때문에 B/E로 가면 parse에서 에러 발생
+ // setTimeout(() => {
+ //     // socket.send("hello from the browser!");
+ //    socket.send(makeMessage("test", "Hi, This is a test msg"));
+ // }, 10000);
+ 
+ 
+ function handleSubmit(event) {
+     event.preventDefault();
+     const input = messageForm.querySelector("input");
+     // JSON으로 전송해서 별명/채팅메시지 구분 위함
+     socket.send(makeMessage("new_message", input.value));
+     // socket.send(input.value);
+     // console.log(input.value);
+     input.value = "";
+ }
+ 
+ function handleNickSubmit(event) {
+     event.preventDefault();
+     const input = nickForm.querySelector("input");
+     // JSON으로 전송해서 닉네임에 해당하는 대화만 뽑기 위함
+     socket.send(makeMessage("nickname", input.value));
+     input.value = "";
+     // socket.send({
+     //     type: "nickname",
+     //     payload: input.value,
+     // });
+ }
+ 
+ messageForm.addEventListener("submit", handleSubmit);
+ nickForm.addEventListener("submit", handleNickSubmit);
+*/
+
+const socket = io();
+
+const welcom = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function backendDone(msg){
+    console.log(`The backend says: `, msg);   
 }
 
-socket.addEventListener("open", () => {
-    console.log("Connected to Server ✅");
-});
-
-socket.addEventListener("message", (message) => {
-    // console.log("Just got this: ", message, "from the server");
-    // console.log("New Message: ", message.data);
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li); 
-});
-
-socket.addEventListener("close", () => {
-    console.log("Disconnected from Server ❌");
-});
-
-// 첫 번째 줄은 JSON 형태가 아니기 때문에 B/E로 가면 parse에서 에러 발생
-// setTimeout(() => {
-//     // socket.send("hello from the browser!");
-//    socket.send(makeMessage("test", "Hi, This is a test msg"));
-// }, 10000);
-
-
-function handleSubmit(event) {
+function handleRoomSubmit(event){
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    // JSON으로 전송해서 별명/채팅메시지 구분 위함
-    socket.send(makeMessage("new_message", input.value));
-    // socket.send(input.value);
-    // console.log(input.value);
+    const input = form.querySelector("input");
+    /** 
+     * socket.send() 해줄 필요 없이 원하는 이벤트를 넘겨주면 됨
+     * Socket IO를 이용하면 Object도 argument로 넘길 수 있다.
+     * 1st argu : event name (same with emit's function name)
+     * 2nd argu : parameter
+     * 3rd argu : callback function
+     * */ 
+    socket.emit("enter_room", { payload:input.value }, backendDone);
     input.value = "";
 }
 
-function handleNickSubmit(event) {
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    // JSON으로 전송해서 닉네임에 해당하는 대화만 뽑기 위함
-    socket.send(makeMessage("nickname", input.value));
-    input.value = "";
-    // socket.send({
-    //     type: "nickname",
-    //     payload: input.value,
-    // });
-}
-
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import http from "http";
+/* WebSocket은 socketIO와 호환되지 않는다.
+socketIO는 websocket의 부가기능이 아님
 import WebSocket from "ws";
+*/
+import SocketIO from "socket.io";
 import express from "express";
+import { Socket } from "dgram";
 
 const app = express();
 
@@ -14,14 +19,25 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 // http 서버 위에 ws 서버를 올려두었기 떄문에 둘 다 사용할 수 있다.
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-// function handleConnnection(socket) {
-//     console.log(socket);
-// }
+const httpServer = http.createServer(app);
+//Socket.io
+const wsServer = SocketIO(httpServer); 
+// WebSocket
+// const wss = new WebSocket.Server({ server });
 
 // wss.on("connection", handleConnnection);
+
+wsServer.on("connection", socket => {
+    socket.on("enter_room", (msg, done)=> {
+        console.log(msg);
+        setTimeout(() => {
+            /* 두번째 인자로 받은 done(); 함수를 실행시킨다.
+                callback 함수 (이름은 상관 없음)
+            */
+            done("hello from the backend");
+        }, 10000);
+    });
+});
 
 function onSocketClose() {
     console.log("Disconnected from the Browser ❌")
@@ -32,6 +48,8 @@ function onSocketMessage(message) {
    console.log(message);
 }
 
+/*
+WebSocket 사용의 경우
 const sockets = [];
 
 wss.on("connection", (socket) => {
@@ -60,6 +78,7 @@ wss.on("connection", (socket) => {
     // wss가 아닌 socket에 있는 메소드를 사용한다.
     // socket.send("hello!!!");
 });
+*/
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
 
